a2cf7d402a32756211f24376dae81f76d290c6c8
Michael Roberts <michael.roberts.jr217@gmail.com>
2019-10-20 14:49:37 -0400

    Jenkinsfile Ultimate Adjustments

--- ppa2src.py
+++ ppa2src.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     try:
         #Database Connection
         db_config = {
-            'host' : '192.168.99.100:8080',
+            'host' : '192.168.99.100',
             'port' : '3306',
             'user' : 'michael',
             'pass' : 'ppa2pass',
